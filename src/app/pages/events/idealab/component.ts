3040069214578d3aabaca61b3c517263e48edaaf
ofwallaart <olaf.wallaart@ecestudents.com>
2018-01-11 10:57:24 +0000

    idealab agenda

--- src/app/pages/events/idealab/component.ts
+++ src/app/pages/events/idealab/component.ts
@@ -18,15 +18,28 @@ export class IdeaLabComponent {
       ];
   
     agenda = [
-            { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
-            { time:"9:00 am - 9:05 am", event:"Testing"},
-            { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
-            { time:"9:00 am - 9:05 am", event:"Testing"},
-            { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
-            { time:"9:00 am - 9:05 am", event:"Testing"},
-            { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
-            { time:"9:00 am - 9:05 am", event:"Testing"},
-            { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
-            { time:"9:00 am - 9:05 am", event:"Testing"},
+            { time:"13:30 START", event: [
+              {text: "Introduction to the event and to ECE Students"},
+              {text: "Company presentations"}
+              ]},
+            { time:"14:15", event:[
+              {text: "Selection of participant groups and assignment of cases; move to workshop rooms"},
+              {text: "Icebreaker games in groups"}
+              ]},
+            { time:"14:45", event:[
+              {text: "Case solving: Brainstorming"},
+              {text: "Case solving: Finalizing solution and preparing pitch"}
+              ]},
+            { time:"16:15", event:[
+              {text: "Groups pitch to startups"}
+              ]},
+            { time:"16:30", event:[
+              {text: "Return to the main room for finalist pitch"},
+              {text: "Selection of winning team by jury"},
+              {text: "Award ceremony and wrap-up"}
+              ]},
+            { time:"17:00 END", event:[
+              
+              ]}
             ];
 }
