2575ee41d99e7a6304328132396334aad881c6d5
ofwallaart <olaf.wallaart@ecestudents.com>
2018-01-21 23:38:51 +0000

    faq and tabs

--- src/app/pages/events/sem/component.ts
+++ src/app/pages/events/sem/component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Core } from 'foundation-sites/js/foundation.core';
+import { Tabs } from 'foundation-sites/js/foundation.tabs';
+import { UtilKeyboard } from 'foundation-sites/js/foundation.util.keyboard';
+import { UtilImageloader } from 'foundation-sites/js/foundation.util.imageLoader';
 
 @Component({
   selector: 'app-sem',
@@ -28,4 +32,17 @@ export class SEMComponent {
             { time:"9:00 am - 9:05 am", event:"Opening Remarks"},
             { time:"9:00 am - 9:05 am", event:"Testing"},
             ];
+    
+    faq = [
+            { ask:"Do I (my team) need to attend all workshops?", answer:"As our competition involves a series of workshops that build up on each other to develop your entrepreneurial idea, attending all workshops is highly advised. Also, the winning team in the final workshop will be determined additionally based on the progress they have made throughout the weeks."},
+            { ask:"I can’t attend a workshop, what should I do?", answer:"In case you cannot come to one of our workshops, please send an email to asparuh.georgiev@ecestudents.com. It is best if at least the majority of your team is present in each workshop."},
+            { ask:"Can I sign up individually?", answer:"Yes, you can! If you don’t have a team, we will find you one based on your indicated interest in the SEM themes. We will group you with participants who have the same preferred theme to work on during the event. "},
+            { ask:"How many people should be in a team?", answer:"All teams should have 4 members to work together. If you have friends/people in mind you would like to work with, please write their names in our application form. All members of your team should fill out the SEM application form. Furthermore, if you do not have enough members for a full team, we will group you with other participants based on best fit. "},
+            { ask:"Is it only for master students?", answer:"We welcome both bachelor and master students. "},
+            { ask:"Why do I need to upload my CV in the registration form?", answer:"We ask you to upload your CV if one of our sponsors or participating companies at SEM would like to know more about you. Furthermore, in the case that there are too many participant applications, we will use the submitted CVs to select who will participate in SEM according to best fit due to  the limited spots available for SEM (maybe we don’t have to mention it now, but we only do the selection if we have too many applications)."},
+            ];
+            
+    ngOnInit() {
+      new Tabs("#tabs");
+    }
 }
