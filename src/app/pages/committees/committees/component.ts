ec0e16ce537da40ea1d1bfbf8a2d1d149e1662a2
Tim <tim@ellemeet.nl>
2018-02-11 01:06:11 +0000

    deleted some crap

--- src/app/pages/committees/committees/component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-committees',
-  templateUrl: './template.html',
-  styleUrls: ['./style.scss']
-})
-export class CommitteesComponent {
-}
