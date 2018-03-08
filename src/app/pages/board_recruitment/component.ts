8e560478ce1f4dce75c94ececd2b32e9cb9ecfa0
Tim <tim@ellemeet.nl>
2018-03-08 16:24:19 +0000

    progress

--- src/app/pages/board_recruitment/component.ts
+++ src/app/pages/board_recruitment/component.ts
@@ -9,5 +9,11 @@ import { Router, NavigationEnd } from '@angular/router';
 
 
 export class BoardRecruitmentComponent {
-
+  positions = [
+    { name: "President", icon: "Lion", text: "" },
+    { name: "Vice President", icon: "Rhino", text: "" },
+    { name: "Programme Manager", icon: "Elephant", text: "" },
+    { name: "External Relations", icon: "Gorilla", text: "" },
+    { name: "Marketing & Tech", icon: "Giraffe", text: "" }
+  ]
 }
