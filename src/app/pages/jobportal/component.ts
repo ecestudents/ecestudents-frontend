3cccb5a3b643a2f8df7f7fdabd37248a4d4761ac
Tim <tim@ellemeet.nl>
2018-03-26 21:30:31 +0000

    okidoki

--- src/app/pages/jobportal/component.ts
+++ src/app/pages/jobportal/component.ts
@@ -20,9 +20,12 @@ export class JobportalComponent implements OnInit {
       gi = d.getElementById,
       ce = d.createElement,
       gt = d.getElementsByTagName,
-      id = "typef_orm"
+      id = "typef_orm",
+      b = "https://embed.typeform.com/";
     if (!gi.call(d, id)) {
+      js = ce.call(d, "script");
       js.id = id;
+      js.src = b + "embed.js";
       q = gt.call(d, "script")[0];
       q.parentNode.insertBefore(js, q)
     }
