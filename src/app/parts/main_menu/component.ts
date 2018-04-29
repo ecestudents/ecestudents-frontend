cc58e9eb934a251b4bc8a7c42cfaffb30c17a7f0
ofwallaart <olaf.wallaart@ecestudents.com>
2018-04-29 16:14:47 +0000

    initial podcast page

--- src/app/parts/main_menu/component.ts
+++ src/app/parts/main_menu/component.ts
@@ -28,6 +28,7 @@ export class MainMenuComponent implements OnInit {
       label: "Events", path: "events", children: this.eventitems
     },
     { label: "Job Portal", path: "jobs" },
+    { label: "Podcast", path: "podcast" },
     // { label: "Board 2018/2019", path: "board2018" }
   ];
 
